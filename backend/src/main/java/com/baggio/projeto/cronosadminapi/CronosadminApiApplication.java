978ed4207200074b6d5f10b7e2b88f80b2f03e69
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-06-11 17:31:12 -0300

    First Commit

---
+++ backend/src/main/java/com/baggio/projeto/cronosadminapi/CronosadminApiApplication.java
@@ -0,0 +1,13 @@
+package com.baggio.projeto.cronosadminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronosadminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronosadminApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/baggio/projeto/cronosadminapi/CronosadminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baggio.projeto.cronosadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronosadminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
